6d72cd57cf70cbc1589cc2c93b68abe067b07294
kimoror <kimoror@mail.ru>
2023-05-06 17:44:49 +0300

    add actuator metrics

--- src/main/java/kimoror/messengeradapter/backend/models/dto/MessageDto.java
+++ src/main/java/kimoror/messengeradapter/backend/models/dto/MessageDto.java
@@ -2,9 +2,11 @@ package kimoror.messengeradapter.backend.models.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto {
   private String requestId;
   private String title;
